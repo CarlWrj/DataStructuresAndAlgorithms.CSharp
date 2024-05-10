42e989e45eed4094360489bc53445187f6bf6b47
吴润杰 <578726973@qq.com>
2024-05-10 16:56:11 +0800

    添加字符串简单模式匹配

---
+++ src/Algorithms/Strings/PatternMatching/SimplePatternMatching.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Strings.PatternMatching
+{
+    /// <summary>
+    /// 简单模式匹配
+    /// </summary>
+    public class SimplePatternMatching
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="str">主串</param>
+        /// <param name="pat">模式串</param>
+        /// <returns></returns>
+        public static int Index(string str, string pat)
+        {
+            var i = 0;
+            var j = 0;
+            while (i < str.Length && j < pat.Length)
+            {
+                if (str[i] == pat[j])
+                {
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    i = i - j + 1;
+                    j = 0;
+                }
+            }
+
+            if (j >= pat.Length)
+            {
+                return i - pat.Length;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
---
+++ tests/Algorithms.Test/Strings/PatternMatching/SimplePatternMatchingTest.cs
@@ -0,0 +1,29 @@
+﻿using Algorithms.Others;
+using Algorithms.Strings.PatternMatching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Test.Strings.PatternMatching
+{
+
+    public class SimplePatternMatchingTest
+    {
+        /// <summary>
+        /// 测试
+        /// </summary>
+        [Fact]
+        public void DoTest()
+        {
+            // Arrange
+            var str = "ABABAcdeABA";
+            var pat = "de";
+            var index = SimplePatternMatching.Index(str, pat);
+
+            // Assert
+            Assert.Equal(6, index);
+        }
+    }
+}
