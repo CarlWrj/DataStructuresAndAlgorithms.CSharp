2e718d44a4c90ed816c564b192e7828c0a5155c2
吴润杰 <578726973@qq.com>
2024-05-14 16:24:17 +0800

    添加KMP算法

---
+++ src/Algorithms/Strings/PatternMatching/KMP.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Strings.PatternMatching
+{
+    /// <summary>
+    /// KMP算法
+    /// </summary>
+    public class KMP
+    {
+        /// <summary>
+        /// 匹配字符串
+        /// </summary>
+        /// <param name="str">主串</param>
+        /// <param name="pat">模式串</param>
+        /// <returns></returns>
+        public static int Index(string str, string pat)
+        {
+            if (str.Length == 1 && pat.Length == 1)
+            {
+                if (str == pat)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+
+            var i = 0;
+            var j = 0;
+            var next = GetNextval(pat);
+
+            while (i < str.Length && j < pat.Length)
+            {
+                if (j == -1 || str[i] == pat[j])
+                {
+                    i++;
+                    j++;
+                }
+                else
+                {
+                    j = next[j];
+                }
+            }
+
+            if (j >= pat.Length)
+            {
+                return i - pat.Length;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        /// <summary>
+        /// 获取Next数组
+        /// </summary>
+        /// <param name="pattern"></param>
+        /// <returns></returns>
+        public static int[] GetNext(string pattern)
+        {
+            var kmpTable = new int[pattern.Length];
+
+            if (kmpTable.Length < 2)
+            {
+                if (kmpTable.Length > 0)
+                    kmpTable[0] = -1;
+
+                return kmpTable;
+            }
+
+            int tableIndex = 2; // current position in table for computation
+            int patSubstrIndex = 0; // index in the pattern of the current substring
+
+            //头2个值固定为-1和0
+            kmpTable[0] = -1;
+
+            // Build table
+            while (tableIndex < kmpTable.Length)
+            {
+                // If the substring continues
+                if (pattern[tableIndex - 1] == pattern[patSubstrIndex])
+                {
+                    kmpTable[tableIndex++] = ++patSubstrIndex;
+                }
+                // It does not but we can fall back
+                else if (patSubstrIndex != 0)
+                {
+                    patSubstrIndex = kmpTable[patSubstrIndex];
+                }
+                // If we ran out of candidates
+                else
+                {
+                    kmpTable[tableIndex++] = 0;
+                }
+            }
+
+            return kmpTable;
+        }
+
+        /// <summary>
+        /// 获取Nextval数组
+        /// </summary>
+        /// <param name="pattern"></param>
+        /// <returns></returns>
+        public static int[] GetNextval(string pattern)
+        {
+            var next = GetNext(pattern);
+            var nextval = new int[pattern.Length];
+            nextval[0] = next[0];
+            for (int i = 1; i < pattern.Length; i++)
+            {
+                if (pattern[next[i]] == pattern[i])
+                {
+                    nextval[i] = nextval[next[i]];
+                }
+                else
+                {
+                    nextval[i] = next[i];
+                }
+            }
+
+            return nextval;
+        }
+    }
+}
---
+++ tests/Algorithms.Test/Strings/PatternMatching/KMPTest.cs
@@ -0,0 +1,56 @@
+﻿using Algorithms.Others;
+using Algorithms.Strings.PatternMatching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Test.Strings.PatternMatching
+{
+
+    public class KMPTest
+    {
+        /// <summary>
+        /// 测试
+        /// </summary>
+        [Fact]
+        public void DoTest()
+        {
+            var str = "a";
+            var pat = "a";
+            var index = KMP.Index(str, pat);
+            Assert.Equal(0, index);
+
+            str = "a";
+            pat = "b";
+            index = KMP.Index(str, pat);
+            Assert.Equal(-1, index);
+
+            str = "ab";
+            pat = "a";
+            index = KMP.Index(str, pat);
+            Assert.Equal(0, index);
+
+            str = "ab";
+            pat = "b";
+            index = KMP.Index(str, pat);
+            Assert.Equal(1, index);
+
+            str = "ab";
+            pat = "c";
+            index = KMP.Index(str, pat);
+            Assert.Equal(-1, index);
+
+            str = "abbababaababbba";
+            pat = "ababaa";
+            index = KMP.Index(str, pat);
+            Assert.Equal(3, index);
+
+            str = "abbababababbba";
+            pat = "ababaa";
+            index = KMP.Index(str, pat);
+            Assert.Equal(-1, index);
+        }
+    }
+}
